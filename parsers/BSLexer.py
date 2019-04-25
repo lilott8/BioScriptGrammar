84c0fde1c65a7ab51df122d010a78ebaf6cf9db2
lilott8 <lilott8@users.noreply.github.com>
2019-04-25 14:48:44 -0700

    Update BSLexer.py

--- parsers/BSLexer.py
+++ parsers/BSLexer.py
@@ -1,4 +1,4 @@
-# Generated from C:/Users/Steven/IdeaProjects/BioScriptGrammar/grammar\BSLexer.g4 by ANTLR 4.7.2
+# Generated from /BioScriptGrammar/grammar\BSLexer.g4 by ANTLR 4.7.2
 from antlr4 import *
 from io import StringIO
 from typing.io import TextIO
